5806c00e80401e27b4fc8138881a87eaababb05a
前野翔平 <fglshm@gmail.com>
2019-07-09 19:26:38 +0900

    commit

---
+++ extensions2/src/main/java/com/fglshm/extensions2/ViewExt.kt
@@ -0,0 +1,11 @@
+package com.fglshm.extensions2
+
+import android.view.View
+
+fun View.setVisible() {
+    this.visibility = View.VISIBLE
+}
+
+fun View.setInvisible() {
+    this.visibility = View.GONE
+}
